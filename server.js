4f6475e2d203a8371d53a5db4d4e1475f436add4
RuqaiahSaif <ruqaiah.saif@gmail.com>
2022-02-16 21:05:30 +0300

    portfolio with nodejs

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+
+app.get('/', (_, res) => {
+    res.status(200).render('index', { title: 'portfolio' });
+});
+
+
+app.use((_, res) => {
+    res.statusCode = 404;
+    res.end('<h1> Sorry, page not found</h1>');
+});
+
+var port = process.env.PORT || 5001
+app.listen(port, () => {
+    console.log('Server running at http://127.0.0.1:5001');
+})
